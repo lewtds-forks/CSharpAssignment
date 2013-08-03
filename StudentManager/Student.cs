105c9c6f81eb85537d4a37f3c4ba58ba94c3026f
Trung Ngo <ndtrung4419@gmail.com>
2013-08-03 14:21:25 +0700

    Remove ClassID property and constructor from class Student

--- StudentManager/Student.cs
+++ StudentManager/Student.cs
@@ -6,16 +6,9 @@ namespace StudentManager
     {
         public int ID { get; set; }
 
-        public int ClassID { get; set; }
-
         public string Name { get; set; }
 
         public string Address { get; set; }
-
-        public Student()
-        {
-
-        }
 
         public override string ToString()
         {
