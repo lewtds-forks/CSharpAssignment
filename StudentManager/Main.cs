6a55216c2fd5341f6fcb4914d6bbb2a1bde8fc7d
Trung Ngo <ndtrung4419@gmail.com>
2013-08-03 17:11:43 +0700

    Update adhoc tests in Main

--- StudentManager/Main.cs
+++ StudentManager/Main.cs
@@ -13,14 +13,28 @@ namespace StudentManager
                 Name = "Thinh",
                 Address = "KT"
             };
+
+            Student chin = new Student()
+            {
+                ID = 5,
+                Name = "Trung",
+                Address = "Home"
+            };
             var m = new Manager();
             var c = new Class()
             {
-                ID = 10
+                ID = 10,
+                Teacher = "NhatNK"
             };
+            m.RegisterClass(c);
             m.AddStudentClass(student, c);
-            Console.WriteLine(student);
+            Console.WriteLine(m.AddStudentClass(chin, c));
+            Console.WriteLine(m.FindStudentByID(5) == null);
 
+            foreach (var s in m.AllClasses)
+            {
+                Console.WriteLine(s.Teacher);
+            }
         }
     }
 }
