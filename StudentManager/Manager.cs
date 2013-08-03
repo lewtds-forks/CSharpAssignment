ca430c88bf51aed9542fb7f96754de7e40a96ac6
Trung Ngo <ndtrung4419@gmail.com>
2013-08-03 14:09:52 +0700

    Formatting

--- StudentManager/Manager.cs
+++ StudentManager/Manager.cs
@@ -4,40 +4,40 @@ using System.Linq;
 
 namespace StudentManager
 {
-	public class Manager
-	{
-		private HashSet<Tuple<Class, Student>> ClassStudentRel;
+    public class Manager
+    {
+        private HashSet<Tuple<Class, Student>> ClassStudentRel;
 
-		public Manager ()
-		{
-			ClassStudentRel = new HashSet<Tuple<Class, Student>> ();
-		}
+        public Manager()
+        {
+            ClassStudentRel = new HashSet<Tuple<Class, Student>>();
+        }
 
-		public bool AddStudentToClass (Student s, Class cl)
-		{
-			return ClassStudentRel.Add (new Tuple<Class, Student> (cl, s));
-		}
+        public bool AddStudentToClass(Student s, Class cl)
+        {
+            return ClassStudentRel.Add(new Tuple<Class, Student> (cl, s));
+        }
 
-		public bool RemoveStudentFromClass(Student s, Class cl)
-		{
-			return ClassStudentRel.Remove (new Tuple<Class, Student> (cl, s));
-		}
+        public bool RemoveStudentFromClass(Student s, Class cl)
+        {
+            return ClassStudentRel.Remove(new Tuple<Class, Student> (cl, s));
+        }
 
-		public bool ChangeClass(Student s, Class frm, Class to)
-		{
-			return this.RemoveStudentFromClass (s, frm) &&
-				this.AddStudentToClass (s, to);
-		}
+        public bool ChangeClass(Student s, Class frm, Class to)
+        {
+            return this.RemoveStudentFromClass(s, frm) &&
+                this.AddStudentToClass(s, to);
+        }
 
-		public Student GetStudentFromID(int ID)
-		{
-			return (from tuple in ClassStudentRel where tuple.Item2.ID == ID select tuple.Item2).FirstOrDefault();
-		}
+        public Student GetStudentFromID(int ID)
+        {
+            return (from tuple in ClassStudentRel where tuple.Item2.ID == ID select tuple.Item2).FirstOrDefault();
+        }
 
-		public Class GetClassFromID(int ID)
-		{
-			return (from tuple in ClassStudentRel where tuple.Item1.ID == ID select tuple.Item1).FirstOrDefault();
-		}
-	}
+        public Class GetClassFromID(int ID)
+        {
+            return (from tuple in ClassStudentRel where tuple.Item1.ID == ID select tuple.Item1).FirstOrDefault();
+        }
+    }
 }
 
--- StudentManager/Student.cs
+++ StudentManager/Student.cs
@@ -2,21 +2,25 @@ using System;
 
 namespace StudentManager
 {
-	public class Student
-	{
-		public int ID { get; set; }
-		public int ClassID { get; set; }
-		public string Name { get; set; }
-		public string Address { get; set; }
+    public class Student
+    {
+        public int ID { get; set; }
 
-		public Student ()
-		{
+        public int ClassID { get; set; }
 
-		}
+        public string Name { get; set; }
 
-		public override string ToString() {
-			return String.Format ("{0} {1} {2} {3}", ID, ClassID, Name, Address);
-		}
-	}
+        public string Address { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3}", ID, ClassID, Name, Address);
+        }
+    }
 }
 
