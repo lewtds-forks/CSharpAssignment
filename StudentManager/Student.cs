9225b32a289842c7af45c26b9ad601571708a51c
Trung Ngo <ndtrung4419@gmail.com>
2013-08-03 16:26:22 +0700

    Implement IComparable and IEquatable for Student

--- StudentManager/Student.cs
+++ StudentManager/Student.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Collections.Generic;
 
 namespace StudentManager
 {
-    public class Student
+    public class Student: IComparable<Student>, IEquatable<Student>
     {
         public int ID { get; set; }
 
@@ -13,6 +14,16 @@ namespace StudentManager
         public override string ToString()
         {
             return String.Format("{0} {1} {2}", ID, Name, Address);
+        }
+
+        public int CompareTo(Student other)
+        {
+            return this.ID - other.ID;
+        }
+
+        public bool Equals(Student other)
+        {
+            return this.ID == other.ID;
         }
     }
 }
