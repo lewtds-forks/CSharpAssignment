1842d70e23c39966a076e14e27f743f2ab384b60
Trung Ngo <ndtrung4419@gmail.com>
2013-08-05 11:19:24 +0700

    Remove previous adhoc tests in Main

--- StudentManager/Main.cs
+++ StudentManager/Main.cs
@@ -7,34 +7,7 @@ namespace StudentManager
     {
         public static void Main(string[] args)
         {
-            Student student = new Student()
-            {
-                ID = 5,
-                Name = "Thinh",
-                Address = "KT"
-            };
 
-            Student chin = new Student()
-            {
-                ID = 5,
-                Name = "Trung",
-                Address = "Home"
-            };
-            var m = new Manager();
-            var c = new Class()
-            {
-                ID = 10,
-                Teacher = "NhatNK"
-            };
-            m.RegisterClass(c);
-            m.AddStudentClass(student, c);
-            Console.WriteLine(m.AddStudentClass(chin, c));
-            Console.WriteLine(m.FindStudentByID(5) == null);
-
-            foreach (var s in m.AllClasses)
-            {
-                Console.WriteLine(s.Teacher);
-            }
         }
     }
 }
