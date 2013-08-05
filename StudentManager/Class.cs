ffa65661018983b4d65c26f3f62e26ded1be954f
Trung Ngo <ndtrung4419@gmail.com>
2013-08-05 09:24:12 +0700

    Add struct Room and struct TimeSlot

--- StudentManager/Class.cs
+++ StudentManager/Class.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 
 namespace StudentManager
 {
+    public struct Room {
+        public String Name { get; set; }
+    }
+
     public struct TimeSlot
     {
-	
+	    public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
     }
 
     public class Class: IComparable<Class>, IEquatable<Class>
