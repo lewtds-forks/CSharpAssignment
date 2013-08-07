70f6648954a0541a5c28d3b05d0082090d2c9f69
Trung Ngo <ndtrung4419@gmail.com>
2013-08-07 15:22:38 +0700

    Add tests

---
+++ StudentManager/Tests/TestIdentity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace StudentManager.Tests
+{
+    [TestFixture]
+    class TestIdentityComparison
+    {
+        [Test]
+        public void TestStudent()
+        {
+            var a = new Student()
+            {
+                ID = 10
+            };
+
+            var b = new Student()
+            {
+                ID = 5
+            };
+
+            var c = new Student() { ID = 10 };
+
+            Assert.That(a.CompareTo(b) > 0);
+            Assert.False(a.Equals(b));
+            Assert.True(a.Equals(c));
+        }
+
+        [Test]
+        public void TestClass()
+        {
+            var a = new Class()
+            {
+                ID = 10
+            };
+
+            var b = new Class()
+            {
+                ID = 5
+            };
+
+            var c = new Class() { ID = 10 };
+
+            Assert.That(a.CompareTo(b) > 0);
+            Assert.False(a.Equals(b));
+            Assert.True(a.Equals(c));
+        }
+    }
+}
---
+++ StudentManager/Tests/TestLogic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace StudentManager.Tests
+{
+    [TestFixture]
+    class TestLogic
+    {
+        private Manager m;
+        private Student trung = new Student()
+        {
+            ID = 10,
+            Name = "Trung"
+        };
+        private Class c1203l = new Class()
+        {
+            ID = 3,
+            Name = "C1203L",
+            Teacher = "NhatNK"
+        };
+
+        [SetUp]
+        public void Init() {
+            m = new Manager();
+        }
+
+        [Test]
+        public void TestGetStudentByID() {
+            Assert.Null(m.GetStudentById(10));
+
+            m.Students.Add(trung);
+            Assert.AreSame(trung, m.GetStudentById(10));
+            Assert.Null(m.GetStudentById(80));
+        }
+
+        [Test]
+        public void TestRegisterStudent() {
+            Assert.False(m.RegisterStudentWithClass(trung, c1203l));
+
+            m.Classes.Add(c1203l);
+            m.Students.Add(trung);
+
+            Assert.True(m.RegisterStudentWithClass(trung, c1203l));
+            var tuple = m.ClassStudents.SingleOrDefault();
+            Assert.NotNull(tuple);
+            Assert.AreSame(tuple.Item1, c1203l);
+            Assert.AreSame(tuple.Item2, trung);
+        }
+
+        [Test]
+        public void TestRemoveStudent() {
+            m.Classes.Add(c1203l);
+            m.Students.Add(trung);
+        }
+    }
+}
