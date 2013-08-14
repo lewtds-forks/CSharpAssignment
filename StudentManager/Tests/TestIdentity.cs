1b784271bcf26bcf70b77cd054f3192c7081d326
Trung Ngo <ndtrung4419@gmail.com>
2013-08-15 02:02:17 +0700

    Fix some test cases

--- StudentManager/Tests/TestIdentity.cs
+++ StudentManager/Tests/TestIdentity.cs
@@ -25,21 +25,20 @@ namespace StudentManager.Tests
             Assert.False(a.Equals(b));
         }
 
-//        [Test]
-//        public void TestClass()
-//        {
-//            var a = new Class()
-//            {
-//                ID = 10
-//            };
-//
-//            var b = new Class()
-//            {
-//                ID = 5
-//            };
-//
-//            Assert.That(a.CompareTo(b) > 0);
-//            Assert.False(a.Equals(b));
-//        }
+        [Test]
+        public void TestClass()
+        {
+            var a = new Class()
+            {
+                Name = "C1203L"
+            };
+
+            var b = new Class()
+            {
+                Name = "C1203F"
+            };
+
+            Assert.False(a.Equals(b));
+        }
     }
 }
--- StudentManager/Tests/TestPersistence.cs
+++ StudentManager/Tests/TestPersistence.cs
@@ -43,7 +43,7 @@ namespace StudentManager
             Assert.AreEqual(
                 @"<?xml version=""1.0"" encoding=""utf-8""?>
 <Student xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
-  <ID>10</ID>
+  <ID>B01414</ID>
   <Name>Trung</Name>
   <Address>Home</Address>
 </Student>",
