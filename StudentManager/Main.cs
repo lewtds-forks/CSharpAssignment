2311735097d9a1d4965819489c05b84e34b5c2cd
Trung Ngo <ndtrung4419@gmail.com>
2013-08-17 02:12:49 +0700

    New Main

--- StudentManager/Main.cs
+++ StudentManager/Main.cs
@@ -9,70 +9,18 @@ namespace StudentManager
     {
         public static void Main(string[] args)
         {
-            Manager m = new Manager();
+            var mapping = new Dictionary<string, string>();
+            mapping.Add("classes", "/tmp/sm/classes.xml");
+            mapping.Add("students", "/tmp/sm/students.xml");
+            mapping.Add("rooms", "/tmp/sm/rooms.xml");
+            mapping.Add("timeslots", "/tmp/sm/timeslots.xml");
+            mapping.Add("class-students", "/tmp/sm/class-students.xml");
+            mapping.Add("allocation", "/tmp/sm/allocation.xml");
 
-            var c1 = new Class()
-            {
-                Name = "C1203L",
-                Teacher = "NhatNK"
-            };
-
-            var c2 = new Class()
-            {
-                Name = "C1204L",
-                Teacher = "SinhNX"
-            };
-
-            var s1 = new Student() {
-                ID = "B01414",
-                Name = "Trung"
-            };
-
-            var s2 = new Student()
-            {
-                ID = "B01415",
-                Name = "Thinh"
-            };
-
-            var r1 = new Room()
-            {
-                Name = "Class 1"
-            };
-
-            var t1 = new TimeSlot()
-            {
-                Name = "night",
-                StartTime = DateTime.MinValue.AddDays(4).AddHours(17).AddMinutes(30),
-                EndTime = DateTime.MinValue.AddDays(4).AddHours(19).AddMinutes(30)
-            };
-
-            m.Classes.Add(c1);
-            m.Classes.Add(c2);
-
-            m.Students.Add(s1);
-            m.Students.Add(s2);
-
-            m.Rooms.Add(r1);
-
-            m.TimeSlots.Add(t1);
-
-            m.RegisterStudentWithClass(s1, c1);
-            m.RegisterStudentWithClass(s2, c2);
-
-            m.RemoveClassRoomTimeSlot(c1, r1, t1);
-
-            m.UriMapping = new Dictionary<string, string>();
-            m.UriMapping.Add("classes", "/tmp/sm/classes.xml");
-            m.UriMapping.Add("students", "/tmp/sm/students.xml");
-            m.UriMapping.Add("rooms", "/tmp/sm/rooms.xml");
-            m.UriMapping.Add("timeslots", "/tmp/sm/timeslots.xml");
-            m.UriMapping.Add("class-students", "/tmp/sm/class-students.xml");
-            m.UriMapping.Add("allocation", "/tmp/sm/allocation.xml");
-//
-            m.database = new XMLDatabase();
-
-            m.Save();
-//            new MainScreen(m).Start();
+            var db = new XMLDatabase();
+            Manager m = new Manager(db, mapping);
+            
+            new MainScreen(m).Start();
         }
     }
 }
