e38bd7ae6aea9c7e05c265bf11f4f6c0e73267aa
Trung Ngo <ndtrung4419@gmail.com>
2013-08-19 16:00:21 +0700

    Remove Room and TimeSlot

--- StudentManager/Class.cs
+++ StudentManager/Class.cs
@@ -3,22 +3,6 @@ using System.Collections.Generic;
 
 namespace StudentManager
 {
-    public class Room : Identity
-    {
-        [Identity.ID]
-        public String Name { get; set; }
-    }
-
-    public class TimeSlot : Identity
-    {
-        [Identity.ID]
-        public String Name {get; set; }
-
-        public DateTime StartTime { get; set; }
-
-        public DateTime EndTime { get; set; }
-    }
-
     public class Class : Identity
     {
         [Identity.ID]
