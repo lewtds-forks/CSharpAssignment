8c444cad05d7d86279abf83ed06855438d7ae434
Trung Ngo <ndtrung4419@gmail.com>
2013-08-20 21:05:54 +0700

    Make database files local

--- StudentManager/Main.cs
+++ StudentManager/Main.cs
@@ -10,12 +10,12 @@ namespace StudentManager
         public static void Main(string[] args)
         {
             var mapping = new Dictionary<string, string>();
-            mapping.Add("classes", "/tmp/sm/classes.xml");
-            mapping.Add("students", "/tmp/sm/students.xml");
-            mapping.Add("rooms", "/tmp/sm/rooms.xml");
-            mapping.Add("timeslots", "/tmp/sm/timeslots.xml");
-            mapping.Add("class-students", "/tmp/sm/class-students.xml");
-            mapping.Add("allocation", "/tmp/sm/allocation.xml");
+            mapping.Add("classes", "classes.xml");
+            mapping.Add("students", "students.xml");
+            mapping.Add("rooms", "rooms.xml");
+            mapping.Add("timeslots", "timeslots.xml");
+            mapping.Add("class-students", "class-students.xml");
+            mapping.Add("allocation", "allocation.xml");
 
             var db = new XMLDatabase();
             Manager m = new Manager(db, mapping);
